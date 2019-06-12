883155a22c05a478db04845184becfc16cd29070
Greg Kapka <greg@oraclize.it>
2019-06-12 16:47:58 +0100

    feat(tests): add tests layout

---
+++ test/provable-zeppelin-crowdsale.test.js
@@ -0,0 +1,10 @@
+const ProvableZeppelinCrowdsale = artifacts.require('ProvableZeppelinCrowdsale')
+
+contract('❍ Provable Zeppelin Crowdsale Tests', ([ _, owner, ...otherAccounts ]) => {
+  it('Should get ETH price in USD on creation')
+  it('Should have initialized the crowdsale after getting ETH price')
+  it('Should calculate exchange rate correctly')
+  it('Should have set exchange rate after getting ETH price')
+  it('Should have a minimum purchase amount of $1')
+  it('Should correctly calculate the amount of tokens $1 gets you')
+})
