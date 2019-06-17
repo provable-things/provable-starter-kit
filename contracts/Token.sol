3e1ad6392160ac4a7a1c3f5d9fac17966ccbcc8a
Greg Kapka <greg@oraclize.it>
2019-06-17 16:25:43 +0100

    feat(token): switch token contract to initializer paradigm

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,20 +1,22 @@
 pragma solidity 0.5.0;
 
+import "zos-lib/contracts/Initializable.sol";
 import '../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
 
-contract Token is ERC20 {
+contract Token is ERC20, Initializable{
 
     string public name;
     string public symbol;
     uint8 public decimals;
     uint256 public initialSupply;
 
-    constructor(
+    function initialize(
         string memory _name,
         string memory _symbol,
         uint8 _decimals,
         uint256 _initialSupply
     )
+        initializer
         public
     {
         name = _name;
