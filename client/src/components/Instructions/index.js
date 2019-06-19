0a3fa45285a61ac00e5508348c212b5144bb8972
Greg Kapka <greg@oraclize.it>
2019-06-19 15:47:50 +0100

    fix(instructions): wrap instructions in Hero layer inside component

--- client/src/components/Instructions/index.js
+++ client/src/components/Instructions/index.js
@@ -1,16 +1,26 @@
+import React from 'react'
 import renderFAQ from './faq'
+import Hero from "../Hero/index.js"
 import renderSetup from './setup-instructions'
+import styles from './Instructions.module.scss'
 import renderMetamask from './metamask-instructions'
 import renderEVMInstructions from './evm-instructions'
 import renderProvableInstructions from './provable-instructions'
 
 export default _props =>
-  _props.name === 'provable'
-    ? renderProvableInstructions(_props)
-    : _props.name === 'evm'
-    ? renderEVMInstructions(_props)
-    : _props.name === 'faq'
-    ? renderFAQ(_props)
-    : _props.name === 'metamask'
-    ? renderMetamask(_props)
-    : renderSetup(_props)
+  (
+    <div className={styles.wrapper}>
+      <Hero />
+      {
+        _props.name === 'provable'
+          ? renderProvableInstructions(_props)
+          : _props.name === 'evm'
+          ? renderEVMInstructions(_props)
+          : _props.name === 'faq'
+          ? renderFAQ(_props)
+          : _props.name === 'metamask'
+          ? renderMetamask(_props)
+          : renderSetup(_props)
+      }
+    </div>
+  )
