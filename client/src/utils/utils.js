51308bcc9e68010696878572b07b8f506c4f0789
Greg Kapka <greg@oraclize.it>
2019-06-19 15:49:14 +0100

    chore(utils): add default address getter to utils

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,3 +1,8 @@
+export const getDefaultAddress = _props =>
+  _props.ganacheAccounts && _props.ganacheAccounts.length > 2
+    ? _props.ganacheAccounts[2]
+    : '<ADDRESS>'
+
 export const waitUntilPageLoaded = _ =>
   new Promise(resolve => window.addEventListener('load', resolve))
 
