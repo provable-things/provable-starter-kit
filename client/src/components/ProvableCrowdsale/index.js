620ff62e4547651ea9a9dc6ee7d6d230b134eeaf
Greg Kapka <greg@oraclize.it>
2019-06-20 15:43:35 +0100

    feat(crowdsale): setup web3 polling in crowdsale...
    ...since now we finally have _only_ the logic we _need_, and it's in the
    right place to use for the DApp.

--- client/src/components/ProvableCrowdsale/index.js
+++ client/src/components/ProvableCrowdsale/index.js
@@ -1,52 +1,61 @@
+import {
+  getGanacheAccounts,
+  getUserBalanceInEth,
+  getContractInstance
+} from '../../utils/utils'
+
+import {
+  getWeb3,
+  getGanacheWeb3,
+  getNetworkIDFromWeb3
+} from '../../utils/get-web3'
+
+import { Loader } from 'rimble-ui'
 import React from 'react'
 import Web3Info from '../Web3Info/index'
 import styles from '../../App.module.scss'
 import DeployCheck from '../DeployCheck/index'
 import StyledLoader from '../StyledLoader/index'
 import Instructions from '../Instructions/index'
+import instructionStyles from '../Instructions/Instructions.module.scss'
+import { zeppelinSolidityHotLoaderOptions } from '../../../config/webpack'
 
-export default _props => {
-  const { hotLoaderDisabled, networkType, accounts, ganacheAccounts } = _props
-  const updgradeCommand = (networkType === 'private' && !hotLoaderDisabled)
-    ? "upgrade-auto"
-    : "upgrade"
-  return (
-    <div className={styles.wrapper}>
-      {!_props.web3 && <StyledLoader />}
-      {
-        _props.web3 &&
-        !_props.contract &&
-         <DeployCheck
-          name='provable'
-          accounts={_props.accounts}
-          ganacheAccounts={_props.ganacheAccounts}
-        />
-      }
-      {_props.web3 && _props.contract && (
-        <div className={styles.contracts}>
-          <h1>Counter Contract is good to Go!</h1>
-          <p>Interact with your contract on the right.</p>
-          <p> You can see your account info on the left </p>
-          <div className={styles.widgets}>
-            <Web3Info { ..._props } />
+export default class Provable extends React.Component {
 
-            {/* <!--The counter stuff goes here --> */}
+  state = {
+    web3: undefined,
+    timeout: null
+  }
 
-          </div>
-          {_props.balance < 0.1 &&
-            <Instructions
-              ganacheAccounts={ganacheAccounts}
-              name="metamask"
-              accounts={accounts} />
-          }
-          {_props.balance >= 0.1 &&
-            <Instructions
-              ganacheAccounts={_props.ganacheAccounts}
-              name={updgradeCommand}
-              accounts={accounts} />
-          }
-        </div>
-      )}
-    </div>
-  )
+  pollForWeb3 = _ =>
+    getNetworkIDFromWeb3(getGanacheWeb3())
+      .then(_web3 => _web3
+        ? this.setState({ web3: _web3 })
+        : Promise.reject()
+      )
+      .catch(_ => {
+        clearTimeout()
+        const timeout = setTimeout(_ => this.pollForWeb3(), 2000)
+        this.setState({ timeout })
+      })
+
+  pollForContracts = _ =>
+    console.log('polling now sir')
+
+  componentWillMount = _ =>
+    this.pollForWeb3()
+
+  clearTimeout = _ =>
+    clearTimeout(this.state.timeout)
+
+  componentWillUnmount = _ =>
+    this.clearTimeout()
+
+  render = _ =>
+    this.state.web3 === undefined
+      ? <Loader size="50px" color="blue" />
+      : this.state.web3 === null
+      ? <Instructions instructionSet='noWeb3' />
+      : <Instructions instructionSet='provableCrowdsale' />
 }
+
