8007edb6d3452b22cf0e0cb260ca63a890375e10
Greg Kapka <greg@oraclize.it>
2019-06-21 11:55:24 +0100

    feat(loader): improve clarity of loading message

--- client/src/components/StyledLoader/index.js
+++ client/src/components/StyledLoader/index.js
@@ -5,6 +5,6 @@ import styles from '../../App.module.scss'
 export default _ =>
   <div className={styles.loader}>
     <Loader size="80px" color="red" />
-    <h3> Loading Web3, accounts, and contract...</h3>
-    <p> Unlock your metamask </p>
+    <h3> Detecting web3 and deployed contracts...</h3>
+    <p> Don't forget to unlock your metamask and point it at localhost!</p>
   </div>
