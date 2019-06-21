cc52ea55aedbe6f62f664c681a0747fb2402b5d7
Greg Kapka <greg@oraclize.it>
2019-06-21 15:45:10 +0100

    fix(compiler-warnings): rm unused components from tutorial

--- client/src/components/Instructions/tutorial-complete.js
+++ client/src/components/Instructions/tutorial-complete.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import { Button } from "rimble-ui"
 import styles from './Instructions.module.scss'
-import { getDefaultAddressFromGanacheAccounts } from '../../utils/utils.js'
 
 export default _props =>
   <div className={styles.instructions}>
